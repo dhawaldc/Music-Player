70ea2aaa26a8870ca69a5393db3d2caaf231c376
dhawaldc <iamdhawal18@gmail.com>
2024-04-19 03:50:01 +0530

    creating playlist

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentSong = new Audio();
 let songs;
+let currFolder;
 function secondsToMinutesSeconds(seconds) {
   if (isNaN(seconds) || seconds < 0) {
     return "00:00";
@@ -13,38 +14,25 @@ function secondsToMinutesSeconds(seconds) {
 
   return `${formattedMinutes}:${formattedSeconds}`;
 }
-async function getSongs() {
-  let a = await fetch("http://127.0.0.1:3000/songs/");
+async function getSongs(folder) {
+  currFolder = folder;
+  let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
   let response = await a.text();
 
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
-  let songs = [];
+  songs = [];
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split("/songs/")[1]);
+      songs.push(element.href.split(`/${folder}/`)[1]);
     }
   }
-  return songs;
-}
-const playMusic = (track, pause = false) => {
-  //let audio = new Audio("/songs/"+track);
-  currentSong.src = "/songs/" + track;
-  if (!pause) {
-    currentSong.play();
-    play.src = "pause.svg";
-  }
-  document.querySelector(".songinfo").innerHTML = decodeURI(track);
-  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
-};
-async function main() {
-  songs = await getSongs();
-  playMusic(songs[0], true);
   let songUL = document
     .querySelector(".songlist")
     .getElementsByTagName("ul")[0];
+  songUL.innerHTML = "";
   for (const song of songs) {
     songUL.innerHTML =
       songUL.innerHTML +
@@ -68,6 +56,67 @@ async function main() {
       playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
     });
   });
+  return songs
+}
+const playMusic = (track, pause = false) => {
+  currentSong.src = `/${currFolder}/` + track;
+  if (!pause) {
+    currentSong.play();
+    play.src = "pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = decodeURI(track);
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+};
+
+async function displayAlbums() {
+  
+  
+  let a = await fetch(`/songs/`);
+  let response = await a.text();
+
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let anchors = div.getElementsByTagName("a");
+  let cardContainer = document.querySelector(".cardContainer");
+  let array = Array.from(anchors);
+  for (let index = 0; index < array.length; index++) {
+    const e = array[index];
+
+    if (e.href.includes("/songs")&& !e.href.includes(".htaccess")) {
+      let folder = e.href.split("/").slice(-2)[0]
+      let a = await fetch(`/songs/${folder}/info.json`)
+      console.log(a);
+      let response = await a.json();
+      console.log(response);
+      cardContainer.innerHTML =
+        cardContainer.innerHTML +
+        `<div data-folder="${folder}" class="card">
+      <div class="play">
+        <img src="play.svg" alt="playbutton" />
+      </div>
+      <img src="/songs/${folder}/cover.jpg"
+        alt=""
+        
+      />
+      <h2>${response.title}</h2>
+      <p>${response.description}</p>
+    </div>`
+    }
+  }
+  Array.from(document.getElementsByClassName("card")).forEach(e => {
+    e.addEventListener("click", async item => {
+      songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
+      playMusic(songs[0])
+    });
+  });
+}
+
+async function main() {
+  await getSongs("songs/ncs");
+  playMusic(songs[0], true);
+  // Display all the albums on the page
+  await displayAlbums();
+
   play.addEventListener("click", () => {
     if (currentSong.paused) {
       currentSong.play();
@@ -122,5 +171,11 @@ async function main() {
     .addEventListener("change", (e) => {
       currentSong.volume = parseInt(e.target.value) / 100;
     });
+  // Load the playlist whenever card is clicked
+  Array.from(document.getElementsByClassName("card")).forEach((e) => {
+    e.addEventListener("click", async (item) => {
+      songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
+    });
+  });
 }
 main();
